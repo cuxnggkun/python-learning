3b528a2177961e03a3f26c8cf0610daf747674c8
cuxnggkun <taihuu299@gmail.com>
2024-07-09 15:40:37 +0700

    update main.py and create demo.py

---
+++ day1/demo.py
@@ -0,0 +1,4 @@
+print("Khi ta lớn lên Đất Nước đã có rồi")
+print("Đất Nước có trong những cái “ngày xửa")
+print("ngày xưa...” mẹ thường hay kể")
+print("Đất Nước bắt đầu với miếng trầu bây giờ")
--- day1/main.py
+++ day1/main.py
@@ -1,1 +1,5 @@
-print("hello world!")
+print("Em tên là : Cường")
+print("Tuổi: 17")
+print("Sở thích: Lập trình")
+print("Điểm mạnh: Không có")
+print("Điểm yếu: Chưa tim ra điểm mạnh")
